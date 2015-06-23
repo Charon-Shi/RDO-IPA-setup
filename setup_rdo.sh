b9fc90f1b862adb6d8278e7a2bfc3731d1e9ebbb
Charon-Shi <chaos.scl@gmail.com>
2015-06-23 15:54:53 -0400

    Update setup_rdo.sh

--- setup_rdo.sh
+++ setup_rdo.sh
@@ -2,6 +2,7 @@
 
 IPA=$1
 
+killall
 hostname rdo.charlie.com
 /etc/init.d/network start
 
@@ -15,6 +16,8 @@ sed -i "1s/^/DNS1=\"$IPA\"\n/" /etc/sysconfig/network-scripts/ifcfg-eth0
 sed -i "s/[0-9.][0-9.]*/$IPA/" /etc/resolv.conf
 
 yum update -y
+service sshd start
 yum install -y https://rdoproject.org/repos/rdo-release.rpm
 yum install -y openstack-packstack
+/usr/bin/rpm -e -nodeps mariadb-server-5.5.41-2.el7_0.x86_64
 packstack --allinone
