074c371d2e4926edad94384bb0c0d782abb6fe2b
Charon-Shi <chaos.scl@gmail.com>
2015-06-24 00:53:47 -0400

    Update setup_rdo.sh

--- setup_rdo.sh
+++ setup_rdo.sh
@@ -11,13 +11,17 @@ systemctl disable NetworkManager
 systemctl enable network
 systemctl start network
 
-sed -i "1s/^/DNS1=\"$IPA\"\n/" /etc/sysconfig/network-scripts/ifcfg-eth0
-/etc/init.d/network start
-sed -i "s/[0-9.][0-9.]*/$IPA/" /etc/resolv.conf
-
 yum update -y
 service sshd start
 yum install -y https://rdoproject.org/repos/rdo-release.rpm
 yum install -y openstack-packstack
 /usr/bin/rpm -e --nodeps mariadb-server-5.5.41-2.el7_0.x86_64
 packstack --allinone
+
+sed -i "1s/^/DNS1=\"$IPA\"\n/" /etc/sysconfig/network-scripts/ifcfg-eth0
+/etc/init.d/network start
+sed -i "s/[0-9.][0-9.]*/$IPA/" /etc/resolv.conf
+yum install ipa-client ipa-admintools
+ipa-client-install --uninstall
+ipa-client-install --force-ntpd --force-join 
+
